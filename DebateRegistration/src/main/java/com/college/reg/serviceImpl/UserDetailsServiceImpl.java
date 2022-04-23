5f2897c6030debc193748c13763297a270a0d473
avisek987 <57721543+avisek987@users.noreply.github.com>
2022-04-24 02:34:53 +0530

    Create UserDetailsServiceImpl.java

---
+++ DebateRegistration/src/main/java/com/college/reg/serviceImpl/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.college.reg.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.college.reg.config.MyUserDetails;
+import com.college.reg.entity.User;
+import com.college.reg.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.getUserByUsername(username);
+
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+
+		return new MyUserDetails(user);
+	}
+
+}

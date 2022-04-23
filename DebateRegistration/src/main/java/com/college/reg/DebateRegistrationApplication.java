e389fa4c54c29c8015cfb8c2898531c603661170
avisek987 <57721543+avisek987@users.noreply.github.com>
2022-04-24 02:23:14 +0530

    Create DebateRegistrationApplication.java

---
+++ DebateRegistration/src/main/java/com/college/reg/DebateRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.college.reg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebateRegistrationApplication.class, args);
+		
+	}
+
+}

9e440bf8850048c40c6a7b9fb18ad56fae9a72e2
avisek987 <57721543+avisek987@users.noreply.github.com>
2022-04-24 02:19:15 +0530

    Create DebateRegistrationApplicationTests.java

---
+++ DebateRegistration/src/test/java/com/college/reg/DebateRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebateRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
